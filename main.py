1c443e20081f2d243cc26ab2b463cdf49b33c895
Nagy Eszter Erzsébet (Szf_N_2024_2026) <nagy.eszter.erzsebet@diak.szamalk-szalezi.hu>
2024-10-24 10:37:24 +0200

    2-es es 3-as feladat

---
+++ main.py
@@ -0,0 +1,12 @@
+import metodosuk
+lista=[5,2,3,4,4,5,5,5,1,2,2,3,4,5,5,5,4,3,3]
+print("2.Összesen hány ötös van? ")
+otos:int=metodosuk.hany_otos(lista,5)
+print(f"Összesen {otos} db ötös van.")
+
+print("3.Összesen hányan kaptak elégtelent?")
+elegtelen:int=metodosuk.hany_otos(lista,1)
+print(f"Összesen {elegtelen} kaptak elégtelent.")
+
+print("Készíts eljárást, mely sávdiagrammal megjeleníti, hogy hány 1-es, hány 2-es, hány 3-as, hány 4-es, hány 5-ös van.")
+
---
+++ metodosuk.py
@@ -0,0 +1,7 @@
+def hany_otos(lista,szam):
+    i:int=0
+    while(i<len(lista)):
+        if(i<lista[i]==szam):
+            szam+=1
+        i+=1
+    return szam
